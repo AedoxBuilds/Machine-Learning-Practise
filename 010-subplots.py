5db3dc0720b7dc550b236af1eeaccc4487df236a
Muhammad Nouman Safeer <rajanoumac8@gmail.com>
2026-09-02 11:23:08 +0500

    Create 010-subplots.py

---
+++ 010-subplots.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+
+#plot 1:
+x = (0, 1, 2, 3)
+y = (3, 8, 1, 10)
+
+plt.subplot(1, 2, 1)
+plt.plot(x,y)
+
+#plot 2:
+x = (0, 1, 2, 3)
+y = (10, 20, 30, 40)
+
+plt.subplot(1, 2, 2)
+plt.plot(x,y)
+
+plt.show()
