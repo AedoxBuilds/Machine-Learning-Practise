2a4c605b21c1488e15d413b028104ddd6c19fbb1
Muhammad Nouman Safeer <rajanoumac8@gmail.com>
2026-09-02 11:22:15 +0500

    Create 008-multiple_lines.py

---
+++ 008-multiple_lines.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+x = [0, 6, 7 ,8 , 9]
+y = [0, 25 , 35, 45, 55]
+
+
+plt.plot(x,y)
+
+
+
+
+
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+plt.title("Sports Watch Data")
+
+plt.show()
